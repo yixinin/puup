b2e0528a25b1bb5959188c63dba33dccc4ebdfd0
eason <yixinin@outlook.com>
2023-03-04 10:14:55 +0800

    update

--- cmd/dll/main.go
+++ cmd/dll/main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
-	"github.com/yixinin/puup/frontend"
-	pnet "github.com/yixinin/puup/net"
+	"C"
+	"fmt"
 )
 
-var sigAddr, serverName string
+// var sigAddr, serverName string
 
-var file *frontend.FileClient
-var hc *http.Client
+// var file *frontend.FileClient
+// var hc *http.Client
+
+// func main() {
+// 	tp, err := pnet.NewTransport(sigAddr, serverName)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	hc = &http.Client{
+// 		Transport: tp,
+// 	}
+// 	file = frontend.NewFileClient(cfg)
+// }
+
+// func Uploadfile(filename string) string {
+// 	fs, err := os.Open(filename)
+// 	if err != nil {
+// 		return err.Error()
+// 	}
+// 	file.CopyFile(file)
+// }
 
 func main() {
-	tp, err := pnet.NewTransport(sigAddr, serverName)
-	if err != nil {
-		panic(err)
-	}
-	hc = &http.Client{
-		Transport: tp,
-	}
-	file = frontend.NewFileClient(cfg)
+
 }
 
-func Uploadfile(filename string) string {
-	fs, err := os.Open(filename)
-	if err != nil {
-		return err.Error()
-	}
-	file.CopyFile(file)
+//export Add
+func Add(a, b *C.int) *C.int {
+	return a + b
+}
+
+//export Hello
+func Hello() {
+	fmt.Println("hello chsarp via go")
 }
